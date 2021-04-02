6315caa16732ba11fc6f40f66d59590e93e0c65f
vatsal28 <vatsalmishra28@gmail.com>
2021-04-02 22:17:08 +0530

    First version with working fixed grid and clear button with a single color

--- etchscript.js
+++ etchscript.js
@@ -1,7 +1,20 @@
+//Create 16x16 grid of divs with class box
 let htmlElements = "";
-
 for (let i = 0; i<256;i++){
     htmlElements+='<div class="box"></div>';
 }
 let container = document.getElementById("container");
 container.innerHTML = htmlElements;
+
+//Function for changing the color of div when hovering
+function changeDefOver(e){
+    e.target.style.backgroundColor = "pink";
+}
+
+//Select all divs having the class box
+let test = document.getElementsByClassName('box');
+
+//Loop over all divs in class box with the mouseover function
+for (let i = 0; i < test.length ; i++){
+    test[i].addEventListener('mouseover',changeDefOver)
+}
